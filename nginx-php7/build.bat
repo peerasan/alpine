be423f8972d31466c4edd5e330fb7413b587f42c
peerasan@gmail.com <peerasan@gmail.com>
2019-10-24 15:46:19 +0700

    nginx-php7: minor fix Dockerfile and nginx, php config

--- nginx-php7/build.bat
+++ nginx-php7/build.bat
@@ -4,5 +4,5 @@ docker stop pat
 docker rm pat
 docker rmi pat
 docker build -t pat .
-docker run --name pat -v F:/GIT/alpine/nginx-php7/apps:/apps -p 80:80 -h lemp -d pat:latest
+docker run --name pat -v d:/dev/alpine/nginx-php7/apps:/apps -p 80:80 -h lemp -d pat:latest
 docker exec -ti pat sh
